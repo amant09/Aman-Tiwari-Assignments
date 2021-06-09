bccc4eb518dee99bfd4143c3be7e63b89b084ffb
Tiwari <aman.tiwari@capgemini.com>
2021-06-09 20:39:38 +0530

    es6 A3

---
+++ ES6 Assignments/A2.js
@@ -0,0 +1,69 @@
+//1.
+class fibo{
+    #previousNo=1;
+    #currentNo=2;
+    next(){
+        return(this.#previousNo+this.#currentNo);
+    }
+}
+let fib=new fibo();
+console.log(fib.next());
+
+ 
+
+//2.
+function nextArmstrongNo(Number){
+
+    console.log ('Armstrong Numbers:');
+    // looping through lowNumber to infinity
+    for (let i = Number; ; i++) {
+        // converting number to string 
+        let numberOfDigits = i.toString().length;
+        let sum = 0;
+        // create a temporary variable
+        let temp = i;
+        /* loop through a number to find if 
+        a number is an Armstrong number */
+        while (temp > 0) {
+            let remainder = temp % 10;
+            sum += remainder ** numberOfDigits;
+            // removing last digit from the number
+            temp = parseInt(temp / 10); // convert float into integer
+        }
+         if (sum == i) {
+            console.log(i);
+            break;
+        }
+    }
+}
+
+ 
+
+//3.
+function nextArmstrongNo(Number){
+
+    console.log ('Armstrong Numbers:');
+    // looping through lowNumber to infinity
+    for (let i = Number;i<=1000 ; i++) {
+        // converting number to string 
+        let numberOfDigits = i.toString().length;
+        let sum = 0;
+        // create a temporary variable
+        let temp = i;
+        /* loop through a number to find if 
+        a number is an Armstrong number */
+        while (temp > 0) {
+            let remainder = temp % 10;
+            sum += remainder ** numberOfDigits;
+            // removing last digit from the number
+            temp = parseInt(temp / 10); // convert float into integer
+        }
+         if (sum == i) {
+            console.log(i);
+            break;
+        }
+        if(i==1000){
+            i=0;
+        }
+    }
+}
--- ES6 Assignments/A3.js
+++ ES6 Assignments/A3.js
@@ -1,69 +1,80 @@
-//1.
-class fibo{
-    #previousNo=1;
-    #currentNo=2;
-    next(){
-        return(this.#previousNo+this.#currentNo);
+//1
+
+var x=0,you=0,sum=0;
+const valOfX= new Promise((resolve)=>{
+    x=2;
+    resolve(x)
+}  )
+
+const valOfY= new Promise((resolve)=>{
+    you=3;
+    resolve(you)
+}  )
+
+Promise.all([valOfX,valOfY]).then(() =>{
+        sum+=x;
+        console.log(x);
+    }).then(() =>{
+         sum+=you;
+        console.log(you);
+    }).then(() =>{
+        console.log(sum)
+    })
+
+
+//2
+
+
+
+class Account{
+  
+    constructor(id, name , balance){
+        this.id = id;
+        this.name = name;
+        this.balance = balance;
+    }
+
+   
+}
+
+class SavingAccount extends Account{
+
+    constructor(id , name , balance , intrest){
+        super(id,name,balance);
+        this.intrest =intrest;
     }
 }
-let fib=new fibo();
-console.log(fib.next());
 
- 
-
-//2.
-function nextArmstrongNo(Number){
-
-    console.log ('Armstrong Numbers:');
-    // looping through lowNumber to infinity
-    for (let i = Number; ; i++) {
-        // converting number to string 
-        let numberOfDigits = i.toString().length;
-        let sum = 0;
-        // create a temporary variable
-        let temp = i;
-        /* loop through a number to find if 
-        a number is an Armstrong number */
-        while (temp > 0) {
-            let remainder = temp % 10;
-            sum += remainder ** numberOfDigits;
-            // removing last digit from the number
-            temp = parseInt(temp / 10); // convert float into integer
-        }
-         if (sum == i) {
-            console.log(i);
-            break;
-        }
+class CurrentAccount extends Account{
+    constructor(id , name , balance , cashCredit){
+        super(id,name,balance);
+        this.cashCredit=cashCredit;
     }
 }
 
- 
+let A1 = new CurrentAccount(101, 'aman', 100, 10);
+let A2 = new CurrentAccount(101, 'rohit', 200 , 5);
+let A3 = new SavingAccount(101, 'nikhil', 150 , 3);
+let A4 = new SavingAccount(101, 'nikki', 250, 7);
 
-//3.
-function nextArmstrongNo(Number){
 
-    console.log ('Armstrong Numbers:');
-    // looping through lowNumber to infinity
-    for (let i = Number;i<=1000 ; i++) {
-        // converting number to string 
-        let numberOfDigits = i.toString().length;
-        let sum = 0;
-        // create a temporary variable
-        let temp = i;
-        /* loop through a number to find if 
-        a number is an Armstrong number */
-        while (temp > 0) {
-            let remainder = temp % 10;
-            sum += remainder ** numberOfDigits;
-            // removing last digit from the number
-            temp = parseInt(temp / 10); // convert float into integer
-        }
-         if (sum == i) {
-            console.log(i);
-            break;
-        }
-        if(i==1000){
-            i=0;
-        }
-    }
+
+
+
+
+//3
+
+interface Printable {
+    name: string,
+    printAll: (string) => string
 }
+var circle: Printable = {
+    dept: "IT",
+    printAll: (str) => { return "Hi" + str }
+}
+var employee: Printable = {
+    name: " xyz",
+    printAll: (str) => { return "Hello" + str }
+}
+console.log(circle.printAll(circle.name))
+console.log(employee.printAll(employee.name))
